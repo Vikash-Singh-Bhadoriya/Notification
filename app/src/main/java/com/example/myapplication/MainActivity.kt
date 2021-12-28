c7751b1ed015ddc4960a98e2b366ab8ac408cf09
Vikash Singh <codingwithvikash@gmail.com>
2021-12-28 20:51:12 +0530

    Added more functionality to the notification
    like Sound, Light, Vibration
    Comments for readability

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,8 +3,12 @@ package com.example.myapplication
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
+import android.content.ContentResolver
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.media.AudioAttributes
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -14,13 +18,15 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    // TODO: 28-12-2021 Understand Broadcast Receiver & add action buttons such as SEE or DISMISS
+
     companion object {
         const val CHANNEL_ID = "NOTIFICATION_CHANNEL_ID_1"
         private const val CHANNEL_NAME = "General notifications"
 
         //for each notification
-        private const val PENDING_INTENT_REQUEST_CODE = 0
-        private const val NOTIFICATION_ID = 0
+        private const val PENDING_INTENT_REQUEST_CODE = 1
+        private const val NOTIFICATION_ID = 1
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,8 +54,11 @@ class MainActivity : AppCompatActivity() {
         // builder defines your actual Notification
         val builder = NotificationCompat.Builder(this, CHANNEL_ID)
             .setSmallIcon(R.drawable.ic_notifications_active_black_24dp)
-            .setContentTitle("Awesome Title")
-            .setContentText("Hey, can you see this damn cool text\nClick to see me")
+            .setContentTitle("Click to see me")
+            .setStyle(
+                NotificationCompat.BigTextStyle()
+                    .bigText("Hii\nHello\nWhat's doing bro\nNothing Much\nSee Me Bro")
+            )
             .setPriority(NotificationCompat.PRIORITY_HIGH)
             .setContentIntent(pendingIntent)
             .setAutoCancel(true)
@@ -58,7 +67,8 @@ class MainActivity : AppCompatActivity() {
             with(NotificationManagerCompat.from(this)) {
                 // notificationId is a unique int for each notification that you must define
                 // if it is the same then it just update the notification with new one
-                notify(NOTIFICATION_ID, builder.build())
+                val notification = builder.build()
+                notify(NOTIFICATION_ID, notification)
             }
         }
     }
@@ -72,7 +82,37 @@ class MainActivity : AppCompatActivity() {
                 CHANNEL_ID,
                 CHANNEL_NAME,
                 NotificationManager.IMPORTANCE_HIGH
-            )
+            ).also {
+                //LIGHT
+                it.enableLights(true)
+                it.lightColor = Color.GREEN
+
+                //SOUND
+                val resourceId = R.raw.birds_notification_sound
+                val soundTrackUri = Uri.Builder()
+                    .scheme(ContentResolver.SCHEME_ANDROID_RESOURCE)
+                    .authority(resources.getResourcePackageName(resourceId))
+                    .appendPath(resources.getResourceTypeName(resourceId))
+                    .appendPath(resources.getResourceEntryName(resourceId))
+                    .build()
+                val audioAttribute = AudioAttributes.Builder()
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                    .build()
+                it.setSound(soundTrackUri, audioAttribute)
+
+                //VIBRATION
+                it.enableVibration(true)
+                // Vibration Pattern if vibration is enabled
+                val vibrationDuration = 1000L
+                val waitingDuration = 2000L
+                it.vibrationPattern = longArrayOf(
+                    waitingDuration,
+                    vibrationDuration,
+                    waitingDuration,
+                    vibrationDuration
+                )
+            }
+
             // Register the channel with the system
             val notificationManager =
                 getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
